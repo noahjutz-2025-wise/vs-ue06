41e39a7b66a32e65a2c671c9f1ebd7df6f4e4ab7
Noah Jutz <noah@noahjutz.com>
2025-12-07 16:24:59 +0100

    Add Adresse.java entity

---
+++ src/main/java/entity/Adresse.java
@@ -0,0 +1,37 @@
+package entity;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Adresse {
+    private String strasse;
+    private String plz;
+    private String ort;
+
+    public String getStrasse() {
+        return strasse;
+    }
+
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+    }
+
+    public String getPlz() {
+        return plz;
+    }
+
+    public void setPlz(String plz) {
+        this.plz = plz;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
+}
