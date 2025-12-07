1d7894ee1910d309cd5591398ee5aad148364a94
Noah Jutz <noah@noahjutz.com>
2025-12-07 16:58:59 +0100

    Add JAX-RS annotations for rest endpoints

--- src/main/java/service/StudentService.java
+++ src/main/java/service/StudentService.java
@@ -8,35 +8,47 @@ import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+@Path("students")
 public class StudentService {
 
   private static final AtomicInteger nextStudentId = new AtomicInteger(1);
 
   private static final ConcurrentMap<Integer, Student> studentDb = new ConcurrentHashMap<>();
 
+  @POST
   public Student matriculate(Student s) {
     final var id = nextStudentId.getAndIncrement();
     s.setStudentNumber(id);
     return studentDb.put(id, s);
   }
 
-  public Student exmatriculate(int studentId) {
+  @DELETE
+  @Path("{id}")
+  public Student exmatriculate(@PathParam("id") int studentId) {
     return studentDb.remove(studentId);
   }
 
-  public Student getStudentById(int studentId) {
+  @GET
+  @Path("{id}")
+  public Student getStudentById(@PathParam("id") int studentId) {
     return studentDb.get(studentId);
   }
 
-  public Student updateStudentAccount(int studentId, Student newData) {
+  @PUT
+  @Path("{id}")
+  public Student updateStudentAccount(@PathParam("id") int studentId, Student newData) {
     return studentDb.put(studentId, newData);
   }
 
+  @GET
+  @Path("all")
   public Collection<Student> getAllStudents() {
     return studentDb.values();
   }
 
-  public Collection<Student> getStudentsByRange(int fromStudentId, int toStudentId) {
+  @GET
+  public Collection<Student> getStudentsByRange(
+      @QueryParam("from") int fromStudentId, @QueryParam("to") int toStudentId) {
     return studentDb.values().stream()
         .filter(s -> s.getStudentNumber() >= fromStudentId && s.getStudentNumber() < toStudentId)
         .collect(Collectors.toSet());
